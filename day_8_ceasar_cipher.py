5e4a58ba244f60dc65e1c3a3d176c46728ccf576
Apostolis Kopalidis <apostkpl@gmail.com>
2024-09-19 18:12:55 +0300

    day 8

--- day_8_ceasar_cipher.py
+++ day_8_ceasar_cipher.py
@@ -2,7 +2,7 @@
 CEASAR CIPHER
 """
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z ']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print("Welcome to the Ceasar Cipher.\nYou can encrypt or decrypt messages\n")
 func = input("Please choose what you\'d like to do, \"encode\" or \"decode\":  ")
@@ -21,5 +21,16 @@ def encrypt(original_text, shift_amount):
             encrypted += " "
     print("Here is the encoded result: " + encrypted)
 
+def decrypt(original_text, shift_amount):
+    decrypted = ""
+    for letter in original_text:
+        if letter != " ":
+            decrypted += alphabet[alphabet.index(letter) - shift_amount]
+        else:
+            decrypted += " "
+    print("Here is the encoded result: " + decrypted)
+
 if func == "encode":
     encrypt(text, shift)
+if func == "decode":
+    decrypt(text, shift)
